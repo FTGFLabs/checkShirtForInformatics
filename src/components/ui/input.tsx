1c154b810d923021077b3e67efd025c7f070ccdc
TAHPAPANGKORN <ththazach@gmail.com>
2025-07-09 09:49:19 +0700

    feat: add input to components

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,25 @@
+'use client'
+import * as React from "react"
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className = "", type = "text", ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        ref={ref}
+        className={
+          "flex h-10 w-full rounded-md border border-black/20 bg-transparent px-4 py-1 text-base shadow-sm transition-colors " +
+          "placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring " +
+          "disabled:cursor-not-allowed disabled:opacity-50 md:text-sm " +
+          className
+        }
+        {...props}
+      />
+    )
+  }
+)
+
+Input.displayName = "Input"
+
+export { Input }
+
