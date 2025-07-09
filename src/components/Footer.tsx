451e0b210f2dc6a9b2971593b465606f260baa8d
TAHPAPANGKORN <papangkorn.pitjawong.work@gmail.com>
2025-07-09 09:50:51 +0700

    chore: remove file .env.example

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,9 @@
 export default function Footer() {
-  const footerDetail = process.env.NEXT_PUBLIC_FOOTER_DETAIL;
 
     return (
       <div className="flex justify-center p-2 gap-2">
         <p className="text-[8px] md:text-[12px] text-gray-400">
-          {footerDetail}
+          ©2025 Technology Development Division, SMOIF, Burapha u.
         </p>
       </div>
     );
