b9ed4e5ce987a27f5a67337cf3bff05f8ad1dbf1
TAHPAPANGKORN <papangkorn.pitjawong.work@gmail.com>
2025-07-09 09:49:19 +0700

    feat: add input to components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 export default function Footer() {
+  const footerDetail = process.env.NEXT_PUBLIC_FOOTER_DETAIL;
 
     return (
       <div className="flex justify-center p-2 gap-2">
         <p className="text-[8px] md:text-[12px] text-gray-400">
-          ©2025 Technology Development Division, SMOIF, Burapha u.
+          {footerDetail}
         </p>
       </div>
     );
